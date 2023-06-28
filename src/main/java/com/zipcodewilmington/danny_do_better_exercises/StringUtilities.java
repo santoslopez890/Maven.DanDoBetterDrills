310ce961c87a00ec3da1b0f883d7a8529c8ad19d
Santos <santos@Zipcoders-MacBook-Pro-9.local>
2023-06-28 08:56:49 -0400

    done

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -8,7 +8,7 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -17,7 +17,7 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+        return firstSegment+secondSegment;
     }
 
     /**
@@ -26,7 +26,7 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+        return firstSegment+secondSegment;
     }
 
     /**
@@ -34,7 +34,7 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+        return input.substring(0,3);
     }
 
     /**
@@ -42,7 +42,7 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        return input.substring(2);
     }
 
     /**
@@ -51,7 +51,7 @@ public class StringUtilities {
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
     public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+        return inputValue==comparableValue;
     }
 
     /**
@@ -59,7 +59,14 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        int middle = 0;
+        if (inputValue.length() % 2 == 0) {
+            middle = inputValue.length() / 2 - 1;
+        }
+        else{
+            middle=inputValue.length()/2;
+        }
+        return (inputValue.charAt(middle));
     }
 
     /**
@@ -67,7 +74,9 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        String arr[] = spaceDelimitedString.split(" ", 2);
+        return arr[0];
+
     }
 
     /**
@@ -75,7 +84,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String arr[] = spaceDelimitedString.split(" ", 2);
+        return arr[1];
     }
 
     /**
@@ -83,6 +93,14 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        char character;
+        String  reversed="";
+        for (int i=0; i<stringToReverse.length(); i++)
+        {
+            character= stringToReverse.charAt(i); //extracts each character
+            reversed= character+reversed; //adds each character in front of the existing string
+        }
+        return reversed;
+
     }
 }
